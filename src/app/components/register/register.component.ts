3ec30b4d5b8248f48d6cd3905f71a94c14dda94f
Emily Potter <emily_p123@hotmail.co.uk>
2019-12-17 18:53:53 +0000

    login working

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -3,7 +3,7 @@ import { ValidateService } from '../../services/validate.service';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-
+import * as bcrypt from 'bcryptjs';
 // const bcrypt = require('bcryptjs');
 
 @Component({
@@ -18,48 +18,49 @@ export class RegisterComponent implements OnInit {
   password: string;
   hashedPassword: string;
 
+  // tslint:disable-next-line:max-line-length
   constructor(private validateService: ValidateService, public authService: AuthService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit() {
   }
 
-  //  public onRegisterSubmit() {
-  //   bcrypt.genSalt(10, (err, salt) => {
-  //     bcrypt.hash(this.password, salt, (ERROR: any, hash: any) => {
-  //       if (ERROR) { throw ERROR; }
-  //       this.hashedPassword = hash;
-  //       const user = {
-  //         name: this.name,
-  //         username: this.username,
-  //         email: this.email,
-  //         password: this.hashedPassword
-  //       };
+   public onRegisterSubmit() {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(this.password, salt, (ERROR: any, hash: any) => {
+        if (ERROR) { throw ERROR; }
+        this.hashedPassword = hash;
+        const user = {
+          name: this.name,
+          username: this.username,
+          email: this.email,
+          password: this.hashedPassword
+        };
 
-  //       // required fields
-  //       if (!this.validateService.validateRegister(user)) {
-  //     this.toastr.error('All fields are required', 'Error');
-  //     this.router.navigate(['/register']);
-  //     return false;
-  //   }
+        // required fields
+        if (!this.validateService.validateRegister(user)) {
+      this.toastr.error('All fields are required', 'Error');
+      this.router.navigate(['/register']);
+      return false;
+    }
 
-  //   // validate email
-  //       if (!this.validateService.validateEmail(user.email)) {
-  //     this.toastr.error('Invalid email address', 'Error');
-  //     this.router.navigate(['/register']);
-  //     return false;
-  //   }
+    // validate email
+        if (!this.validateService.validateEmail(user.email)) {
+      this.toastr.error('Invalid email address', 'Error');
+      this.router.navigate(['/register']);
+      return false;
+    }
 
-  //   // // register user
-  //       this.authService.registerUser(user).subscribe(() => {
-  //     this.router.navigate(['']).then(() => {
-  //       this.toastr.success('Success', 'User Registered');
-  //     });
-  //   }, error => {
-  //     this.toastr.error('Error', 'Unable to register user');
-  //     this.router.navigate(['/register']);
-  //     console.log(error);
-  //   });
-  //     });
-  //   });
-  // }
+    // // register user
+        this.authService.registerUser(user).subscribe(() => {
+      this.router.navigate(['']).then(() => {
+        this.toastr.success('Success', 'User Registered');
+      });
+    }, error => {
+      this.toastr.error('Error', 'Unable to register user');
+      this.router.navigate(['/register']);
+      console.log(error);
+    });
+      });
+    });
+  }
 }
