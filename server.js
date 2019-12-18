9d45af9d5a1da38ebb4dece8b9ee3968c35f0d38
Emily Potter <emily_p123@hotmail.co.uk>
2019-12-18 17:43:51 +0000

    try to correct cors issue

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/swell'));
 
 app.get('/*', function(req,res) {
+
+// CORS middleware
+app.use(cors());
     
 res.sendFile(path.join(__dirname+'/dist/swell/index.html'));
 });
